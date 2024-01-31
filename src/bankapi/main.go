109c28efaf20a471b7157449aa7e3e7a15ffa40e
Geoff1900 <norton_geoff@hotmail.com>
2024-01-31 17:12:23 +0000

    feat: Added in-memory database

--- src/bankapi/main.go
+++ src/bankapi/main.go
@@ -8,4 +8,15 @@ import (
 
 func main() {
 	fmt.Println(bank.Ping())
+	var accounts = map[float64]*bank.Account{}
+
+	accounts[1001] = &bank.Account{
+		Customer: bank.Customer{
+			Name:    "John",
+			Address: "Los Angeles, California",
+			Phone:   "(213) 555 0147",
+		},
+		Number: 1001,
+	}
+
 }
